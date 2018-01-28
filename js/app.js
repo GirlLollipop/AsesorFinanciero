9c5844d73222022d461eba3414d153a9998274df
GirlLollipop <girl.lollipop@gmail.com>
2018-01-28 11:50:04 -0600

    First commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+   setTimeout("$('.splash').hide();", 2000);
+});
+
+//ofertas
+$(document).ready(function() {
+    $('select').material_select();
+  });
+$('select').material_select('destroy');
+
+//modal add card
+$(document).ready(function(){
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal-trigger').leanModal();
+ });
